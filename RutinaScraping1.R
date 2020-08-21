b877941a7229bc9ff08d052e51b52c95279cb71f
SergioPino98 <se.pino@uc.cl>
2020-08-21 13:10:38 -0400

    Rutina 1 Scraping

---
+++ RutinaScraping1.R
@@ -0,0 +1,21 @@
+# General-purpose data wrangling
+library(tidyverse)  
+
+# Parsing of HTML/XML files  
+library(rvest)    
+
+# String manipulation
+library(stringr)   
+
+# Verbose regular expressions
+library(rebus)     
+
+# Eases DateTime manipulation
+library(lubridate)
+
+url <-'http://www.trustpilot.com/review/www.amazon.com'
+
+a<-2
+b<-3
+c<-a+b
+print(c)
